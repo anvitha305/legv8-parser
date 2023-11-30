d8fe966ce86f8d89c69d9cba57de82629e5acac7
Anvitha <anvitha2k20@gmail.com>
2023-11-30 05:04:12 -0500

    add in all support for parsing instructions.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,113 @@
 use nom::{
   IResult,
   multi::{many0, many1},
-  combinator::{map_res, verify, value, opt, recognize},
-  sequence::{preceded, pair, delimited, tuple, terminated},
+  combinator::{verify, value, recognize},
+  sequence::{preceded, pair, delimited, terminated},
   character::complete::{char, digit1, one_of},
-  branch::{alt},
-  bytes::complete::{tag, is_not},
+  branch::alt,
+  bytes::complete::{tag, is_not, tag_no_case},
 };
 pub fn main(){
-  print!("{:#?}", parse("//ldur [x1, x3]"))
+  print!("{:#?}", parse("add x1, x3, x4"))
   //dtype("ldus[x1, x3]")
+}
+
+// recognizes all r-type instructions 
+pub fn rtype(input: &str) -> IResult<&str, &str> {
+  alt((intrtype, fltrtype))(input)
+}
+
+// recognizes r-type operators/instructions for integers
+pub fn intrtype(input: &str) -> IResult<&str, &str> {
+  alt(
+  (tag_no_case("add"),
+  tag_no_case("adds"),
+  tag_no_case("and"),
+  tag_no_case("ands"),
+  tag_no_case("br"),
+  tag_no_case("eor"),
+  tag_no_case("lsl"),
+  tag_no_case("lsr"),
+  tag_no_case("orr"),
+  tag_no_case("sub"),
+  tag_no_case("subs")))
+  (input)
+}
+
+// recognizes r-type operators/instructions for floats
+pub fn fltrtype(input: &str) -> IResult<&str, &str> {
+  alt(
+  (tag_no_case("fadd"),
+  tag_no_case("fcmpd"),
+  tag_no_case("fdivd"),
+  tag_no_case("fmuld"),
+  tag_no_case("fsubd"),
+  tag_no_case("mul"),
+  tag_no_case("sdiv"),
+  tag_no_case("smulh"),
+  tag_no_case("udiv"),
+  tag_no_case("umulh")))
+  (input)
+}
+
+// recognizes i-type operators/instructions
+pub fn itype(input: &str) -> IResult<&str, &str> {
+  alt(
+    (tag_no_case("addi"),
+    tag_no_case("andi"),
+    tag_no_case("andis"),
+    tag_no_case("eori"),
+    tag_no_case("orri"),
+    tag_no_case("subi"),
+    tag_no_case("subis")))
+    (input)
+}
+
+// recognizes d-type operators/instructions
+pub fn dtype(input: &str) -> IResult<&str, &str> {
+  alt(
+    (tag_no_case("ldur"),
+    tag_no_case("ldursw"),
+    tag_no_case("stur"),
+    tag_no_case("lda"),
+    tag_no_case("mov")))
+    (input)
+}
+
+
+// recognizes cb-type instructions
+pub fn cbtype(input: &str) -> IResult<&str, &str> {
+  alt(
+    (tag_no_case("cbnz"),
+    tag_no_case("cbz"),
+    tag_no_case("b.le"),
+    tag_no_case("b.lt"),
+    tag_no_case("b.eq"),
+    tag_no_case("b.ne"),
+    tag_no_case("b.ge"),
+    tag_no_case("b.gt")))
+    (input)
+}
+
+// recognizes b-type instructions
+pub fn btype(input: &str) -> IResult<&str, &str> {
+  alt(
+    (tag_no_case("b"),
+    tag_no_case("bl")))
+    (input)
+}
+
+// flag updater instructions
+pub fn flagtype(input: &str) -> IResult<&str, &str> {
+  alt(
+    (tag_no_case("cmp"),
+    tag_no_case("cmpi")))
+    (input)
 }
 
 // recognizes brackets for d-type instructions 
 pub fn brack(input: &str) -> IResult<&str, &str> {
-    delimited(char('['), is_not("]"), char(']'))(input)
+    delimited(char('['), is_not("[]"), char(']'))(input)
 }
 
 // recognizes comments 
@@ -99,5 +192,5 @@ pub struct Branch{
   pub inst: Vec<Instruction>
 }
 pub fn parse(code: &str)->IResult<&str,&str>{
-  alt((reg, comment, imm, brack))(code)
+  alt((reg, comment, imm, brack, rtype))(code)
 }

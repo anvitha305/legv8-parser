67b276175db396cb6e699209ca53612d3b19e048
Anvitha <anvitha2k20@gmail.com>
2024-01-04 19:19:57 -0500

    a lot of fixing of main to get the parser almost done 💀

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,61 @@
 use nom::{
   IResult,
-  multi::{many0, many1},
-  combinator::{verify, value, recognize},
+  multi::{many0, many1, separated_list1},
+  combinator::{verify, value, recognize, map_res},
   sequence::{preceded, pair, delimited, terminated},
-  character::complete::{char, digit1, one_of},
+  character::complete::{char, digit1, one_of, multispace1},
   branch::alt,
-  bytes::complete::{tag, is_not, tag_no_case},
+  bytes::complete::{tag, is_not, tag_no_case}, Parser,
 };
 pub fn main(){
-  print!("{:#?}", parse("add x1, x3, x4"))
+  print!("{:#?}", d_inst("ldur x2 [x3, #5]"))
   //dtype("ldus[x1, x3]")
 }
 
 // recognizes all r-type instructions 
 pub fn rtype(input: &str) -> IResult<&str, &str> {
   alt((intrtype, fltrtype))(input)
+
+}
+
+// for parsing r-type instructions into IR.
+pub fn r_inst(input: &str) -> IResult<&str, Instruction>{
+  let (input, instr) = rtype(input)?; 
+  let (input, _) = tag(" ")(input)?;
+  let (input, regs)  = separated_list1(alt((tag(", "), tag(","))), reg)(input)?;
+  Ok((input, Instruction{typ:Typ::R, instr:instr.to_string(), regs:regs, addr:0, imm:0, bname:"".to_string()}))
+}
+
+// for parsing i-type instructions into IR.
+pub fn i_inst(input: &str) -> IResult<&str, Instruction>{
+  let (input, instr) = itype(input)?; 
+  let (input, _) = tag(" ")(input)?;
+  let (input, regs)  = separated_list1(alt((tag(", "), tag(","))), reg)(input)?;
+  let (input,_) = alt((tag(", "), tag(",")))(input)?;
+  let (input, imm) = imm(input)?;
+  Ok((input, Instruction{typ:Typ::I, instr:instr.to_string(), regs:regs, addr:0, imm:imm, bname:"".to_string()}))
+}
+
+pub fn d_inst(input: &str) -> IResult<&str, Instruction>{
+  let (input, instr) = dtype(input)?;
+  let (input, _) = tag(" ")(input)?;
+  let (input, mut regs)  = separated_list1(alt((tag(", "), tag(","))), reg)(input)?;
+  let (input, _) = many0(tag(" "))(input)?;
+  if instr.to_lowercase() == "mov" {
+    return Ok((input, Instruction{typ:Typ::D, instr:instr.to_string(), regs:regs, addr:0, imm:0, bname:"".to_string()}))
+
+  }
+  let (input, _lbrack) = tag("[")(input)?;
+  let (input, reg1) = reg(input)?;
+  let mut regs2 = vec![reg1];
+  regs.append(&mut regs2);
+  if instr.to_lowercase() == "lda" {
+    let (input, _rbrack) = tag("]")(input)?;
+    return Ok((input, Instruction{typ:Typ::D, instr:instr.to_string(), regs:regs, addr:0, imm:0, bname:"".to_string()}))
+  }
+  let (input, _) = alt((tag(", "), tag(",")))(input)?;
+  
+  Ok((input, Instruction{typ:Typ::D, instr:instr.to_string(), regs:regs, addr:0, imm:0, bname:"".to_string()}))
 }
 
 // recognizes r-type operators/instructions for integers
@@ -75,6 +116,7 @@ pub fn dtype(input: &str) -> IResult<&str, &str> {
 }
 
 
+
 // recognizes cb-type instructions
 pub fn cbtype(input: &str) -> IResult<&str, &str> {
   alt(
@@ -105,9 +147,13 @@ pub fn flagtype(input: &str) -> IResult<&str, &str> {
     (input)
 }
 
+pub fn offset(input: &str) -> IResult<&str, &str>{
+  delimited(reg, alt((tag(", "), tag(","))), imm)(input)
+}
+
 // recognizes brackets for d-type instructions 
 pub fn brack(input: &str) -> IResult<&str, &str> {
-    delimited(char('['), is_not("[]"), char(']'))(input)
+    delimited(char('['),offset, char(']'))(input)
 }
 
 // recognizes comments 
@@ -124,22 +170,22 @@ pub fn comment(input: &str) -> IResult<&str, &str> {
 
 
 // recognizes values we know immediately
-pub fn imm(input: &str) -> IResult<&str, &str> {
-      preceded(
+pub fn imm(input: &str) -> IResult<&str, u64> {
+      map_res(preceded(
         tag("#"),
         recognize(
           many1(
             terminated(one_of("0123456789"), many0(char('_')))
           )
         )
-      )(input)
+      ), |out: &str|out.to_string().replace("#", "").parse()).parse(input)
 }
 
 // recognizes one of the numbered registers
 fn numreg(input: &str) -> IResult<&str, &str> {
     recognize(
       pair(
-        tag("x"), 
+        tag_no_case("x"), 
         verify(
           digit1, 
           |s: &str| (0..31).contains(&(s.parse().unwrap())
@@ -152,12 +198,12 @@ fn numreg(input: &str) -> IResult<&str, &str> {
 // recognizes one of the named registers and converts it to the numbered registers
 fn altreg(input: &str) -> IResult<&str, &str> {
     alt((
-      value("x16", tag("ip0")),
-      value("x17", tag("ip0")),
-      value("x28", tag("sp")), 
-      value("x29", tag("fp")), 
-      value("x30", tag("lr")), 
-      value("x31", tag("xzr"))
+      value("x16", tag_no_case("ip0")),
+      value("x17", tag_no_case("ip0")),
+      value("x28", tag_no_case("sp")), 
+      value("x29", tag_no_case("fp")), 
+      value("x30", tag_no_case("lr")), 
+      value("x31", tag_no_case("xzr"))
     )
   )(input)
 }
@@ -170,6 +216,9 @@ pub fn reg(input: &str) -> IResult<&str, &str> {
   )
   )(input)
 }
+pub fn parseall(input:&str)-> IResult<&str, &str>{
+  alt((reg, comment, brack, rtype, tag(",")))(input)
+}
 
 
 // Type of instruction being used.
@@ -179,25 +228,29 @@ pub fn reg(input: &str) -> IResult<&str, &str> {
 // B: B-type, unconditional branching
 // C: CB-type, conditional branching
 // M: IM-type, moving shifted immediate to register
-
+#[derive(Debug)]
 pub enum Typ {R, I, D, B, C, M}
 
-pub struct Instruction{
+#[derive(Debug)]
+pub struct Instruction<'a>{
 	pub typ: Typ,
 	pub instr: String,
-  pub regs: Vec<String>,
-  pub addr: u16    
+  pub regs: Vec<&'a str>,
+  pub addr: u64,   
+  pub imm: u64,
+  pub bname: String,
 }
 
-pub struct Branch{
+
+pub struct Branch<'a>{
   pub name: String,
-  pub inst: Vec<Instruction>
+  pub inst: Vec<Instruction<'a>>
 }
-pub fn parse(code: &str)->IResult<&str,&str>{
+pub fn parse(code: &str){
   let lines = code.split("\n");
   for line in lines {
-    println!(line)
-    println!(alt((reg, comment, imm, brack, rtype))(line))
+    println!("{:?}", line);
+    println!("{:#?}", many0(alt((reg, dtype, comment, brack, rtype, multispace1, tag(","))))(line))
   }
   
 }
